4c9d20c4acfda5602479114a96c732f65ab217f5
freeluncersagar <freeluncersagar@gmail.com>
2024-03-06 21:23:13 +0600

    Hi

---
+++ index.php
@@ -0,0 +1,313 @@
+<? php 
+
+$num1 = 500;
+$num2 = 700;
+//num3 = $num1 + $num2;
+
+//echo num3;
+ 
+function sum()
+{
+	global $num1, $num2;
+	echo $num2 . "</br>";
+}
+	
+	sum()
+	$num3 = $num1 + $num2;
+	echo = num3;
+
+
+	$num4 = 100;
+	$num5 = 600;
+	function total()
+	{
+		global $num4, $num5;
+		echo $num4 ."</br>";
+		echo $num5. "</br>";
+}
+	 total()
+	 $num7 = $num4 + $num5;
+	 echo $num7; 
+
+
+
+
+
+
+
+$name = Sonia;
+$employeeid = 02;
+echo $name ."</br>";
+echo $employeeid ."</br>";
+
+sprintf("$name: %s , $employeeid: %d", $name, $employeeid);
+$employee_info = sprintf("$name: %s, $employeeid: %d", $name, $employeeid);
+
+$name = Tania Azad;
+$employeeid = 203;
+
+echo $name . "</br>";
+echo $employeeid ."</br>";
+
+sprintf("$name: %s, $employeeid: %d", $name, $employeeid);
+$employee_info = sprintf("$name: %s, employeeid:%d", $name,$employeeid);
+
+
+$name = Sonia;
+$employeeid = 02;
+
+echo "$name";
+echo "$employeeid";
+
+sprintf("$name: %s, $employeeid:%d", $name, $employeeid);
+$employee_info =sprintf("$name:%s, $employeeid:%d", $name,$employeeid);
+
+
+
+	$num1 =20;
+	$num2 =30;
+	$addition = $num1+$num2;
+
+	echo $sum;
+
+	function addition()
+	{
+		global $num1, $num2;
+		echo $num1 ."</br>";
+		echo $num2."</br>";
+	}
+	addition()
+
+	$total = $num1+$num2;
+	echo "$total";
+
+
+	$name = Sonia;
+	$employee_id =02;
+
+	printf("$name: %s, employee_id:%d", $name,$employeeid);
+
+
+	$num=0;
+	echo ($num%2);
+
+
+
+
+	$age = 27;
+	if($age>19)
+	{
+		echo "Can try for work."; 
+	}
+	else
+	{
+		echo "Wait untill appropriate age.";
+
+	}
+
+	$num = 30;
+	if($num%2==0 && $num>45)
+		echo "This is even Number";
+	else
+	{
+		echo "Nothing";
+	}
+
+	$num =30;
+	if($num&2==0 || $num>60)
+	{
+		echo "jshdgfj";
+	}
+else
+{
+	echo "gfsfdgf";
+} 
+
+
+//$num1 = 20;
+//$num2 = 30;
+//echo "num3 = $num1+$num2";
+/* $number = 40;
+ if($number%2==0 && $number<20)
+ {
+ 	echo"This is even Number";
+ }
+ else
+ {
+ 	echo"Something other";
+ }
+*/
+
+ $num3 = 20;
+ $num0 = 30;
+
+function total()
+{
+	global $num3, $num4;
+	echo "$num3";
+	echo "$num0";
+}
+total()
+$sum = $num3+$num0;
+echo "$sum";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
